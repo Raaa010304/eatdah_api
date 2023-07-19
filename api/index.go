e2c02952eb8329716d974e7f138ea30aa14f9d13
Raaa010304 <129659838+Raaa010304@users.noreply.github.com>
2023-07-20 04:36:00 +0700

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,46 +1,29 @@
-
 package handler
 
 import (
-	"fmt"
-	"net/http"
+	"eatdah/controllers/menucontroller"
+	"eatdah/controllers/menufavoritecontroller"
+	"eatdah/models"
 
-	. "github.com/tbxark/g4vercel"
+	"github.com/gin-gonic/gin"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	server := New()
+func Handler() {
+	server := gin.Default()
 
-	server.GET("/", func(context *Context) {
-		context.JSON(200, H{
-			"message": "hello go from vercel !!!!",
-		})
-	})
-	server.GET("/hello", func(context *Context) {
-		name := context.Query("name")
-		if name == "" {
-			context.JSON(400, H{
-				"message": "name not found",
-			})
-		} else {
-			context.JSON(200, H{
-				"data": fmt.Sprintf("Hello %s!", name),
-			})
-		}
-	})
-	server.GET("/user/:id", func(context *Context) {
-		context.JSON(400, H{
-			"data": H{
-				"id": context.Param("id"),
-			},
-		})
-	})
-	server.GET("/long/long/long/path/*test", func(context *Context) {
-		context.JSON(200, H{
-			"data": H{
-				"url": context.Path,
-			},
-		})
-	})
-	server.Handle(w, r)
+	models.ConnectDatabase()
+
+	server.GET("/api/menus", menucontroller.Index)
+	server.GET("/api/menu/:id", menucontroller.Show)
+	server.POST("/api/menu", menucontroller.Create)
+	server.PUT("/api/menu/:id", menucontroller.Update)
+	server.DELETE("/api/menu", menucontroller.Delete)
+
+	server.GET("/api/menusfavorite", menufavoritecontroller.Index)
+	server.GET("/api/menufavorite/:id", menufavoritecontroller.Show)
+	server.POST("/api/menufavorite", menufavoritecontroller.Create)
+	server.PUT("/api/menufavorite/:id", menufavoritecontroller.Update)
+	server.DELETE("/api/menufavorite", menufavoritecontroller.Delete)
+
+	server.Run()
 }
